507d36257a1fdeb7fbc5fbbb3d5e7740292b2b11
Daniel Andersson <danzigdanne@gmail.com>
2019-10-28 13:38:58 +0100

    Add project files.

---
+++ DaysOfHackerrank/PowerException.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DaysOfHackerrank
+{
+
+
+
+
+    class PowerException : Exception
+    {
+        public override string Message
+        {
+            get { return "n and p should be non-negative"; }
+
+        }
+
+
+        //Write your code here
+        public Stack<char> stack = new Stack<char>();
+        public Queue<char> queue = new Queue<char>();
+
+        public void pushCharacter(char ch)
+        {
+            stack.Push(ch);
+        }
+        public void enqueueCharacter(char ch)
+        {
+            queue.Enqueue(ch);
+        }
+        public char popCharacter()
+        {
+            return stack.Pop();
+        }
+        public char dequeueCharacter()
+        {
+            return queue.Dequeue();
+        }
+        static int[] cutTheSticks(int[] arr)
+        {
+            Array.Sort(arr);
+            
+            List<int> sticksCut = new List<int>();
+
+            // När Count blir noll ska loopen sluta.
+            int lowest = -1;
+            while (lowest +1 < arr.Length)
+            {
+
+                lowest++;
+                sticksCut.Add((arr.Length - lowest) -1);
+
+
+                for (int i = lowest; i < arr.Length; i++)
+                {
+                    if (arr[lowest] == arr[i])
+                    {
+                        lowest++;
+                    }
+                }
+
+            }
+
+            return sticksCut.ToArray();
+        }
+
+
+        public void printArray<T>(T[] array)
+        {
+            foreach (var s in array){
+                Console.WriteLine(s);
+            }
+        }
+    }
+}
---
+++ DaysOfHackerrank/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DaysOfHackerrank
+{
+    class Program
+    {
+
+        static long L = 2000000;
+        static bool[] notPrime = new bool[L];
+
+        static void Main(String[] args)
+        {
+            /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution */
+
+            findPrimes(2);
+
+            notPrime[0] = true;
+            notPrime[1] = true;
+            //notPrime[2] = true;
+            for (int i = 3; i < 200; i += 2)
+            {
+                for (int j = 1; j <= (i / 2); j++)
+                {
+                    if (i % j == 0)
+                    {
+                        continue;
+                    };
+                }
+                //notPrime[i] = false;
+                findPrimes(i);
+            }
+
+            int T = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < T; i++)
+            {
+                int response = Convert.ToInt32(Console.ReadLine());
+                string text = "";
+                long now = DateTime.Now.Millisecond;
+                text = notPrime[response] ? "Not prime" : "Prime";
+                
+                Console.WriteLine(text);
+                long past = DateTime.Now.Millisecond;
+                Console.WriteLine(past-now);
+                past = DateTime.Now.Millisecond;
+
+                text = !IsPrime(response) ? "Not prime" : "Prime";
+                now = DateTime.Now.Millisecond;
+
+                Console.WriteLine(text);
+                Console.WriteLine(now-past);
+            }
+        }
+
+        public static bool IsPrime(int number)
+        {
+            if (number <= 1) return false;
+            if (number == 2) return true;
+            if (number % 2 == 0) return false;
+
+            var boundary = (int)Math.Floor(Math.Sqrt(number));
+
+            for (int i = 3; i <= boundary; i += 2)
+                if (number % i == 0)
+                    return false;
+
+            return true;
+        }
+
+
+        static void findPrimes(int adding)
+        {
+
+            for (long b = adding * 2; b < L; b += adding)
+            {
+                notPrime[b] = true;
+            }
+        }
+    }
+    }
